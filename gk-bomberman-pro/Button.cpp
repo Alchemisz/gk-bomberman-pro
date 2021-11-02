5e3048124b38d7636328a207c98b88b50841fa5f
Adrian Rubak <adek.rubak@gmail.com>
2021-11-02 19:13:22 +0100

    Trzeba dodać fonty!

--- gk-bomberman-pro/Button.cpp
+++ gk-bomberman-pro/Button.cpp
@@ -1,7 +1,11 @@
 #include "Button.h"
 #include "Engine.h"
+
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 void Button::draw()
 {
+
 	al_draw_filled_rectangle(this->x, this->y, this->x + Button::BUTTON_WIDTH,  this->y + Button::BUTTON_HEIGHT, this->frameColor);
 	al_draw_filled_rectangle(this->x + FRAME_MARGIN,
 							this->y + FRAME_MARGIN,
@@ -9,6 +13,8 @@ void Button::draw()
 							this->y + Button::BUTTON_HEIGHT - FRAME_MARGIN,
 							this->buttonColor);
 
+	//al_draw_text(font, Color::Black, this->x + (Button::BUTTON_WIDTH / 2),
+	//	(this->y + Button::BUTTON_HEIGHT / 2), ALLEGRO_ALIGN_CENTRE, this->text.c_str());
 
 }
 
